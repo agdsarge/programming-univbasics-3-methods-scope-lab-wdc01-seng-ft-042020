b504aef4160d72c4b79340f8270e710de775e34b
Andrew Sargeant <agdsargeant@gmail.com>
2020-03-24 16:35:02 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,30 @@
+#mp = "It's-a me, Mario!\n"
+#tp = "Thank You Mario! But Our Princess Is In Another Castle!\n"
+#lp = "It's Dangerous To Go Alone! Take This.\n"
+
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+  puts "It's-a me, Mario!\n"
 end
 
 def toadstool
-  puts status
+  puts "Thank You Mario! But Our Princess Is In Another Castle!\n"
 end
+
+def link
+  puts "It's Dangerous To Go Alone! Take This.\n"
+
+#def all_phrases(character)
+  #case character
+   # when "mario"
+    #  mario
+    #when "toadstool"
+      #toadstool
+    #when "link"
+     # link
+  #end
+#end
+
+
+#all_phrases("mario")
+#all_phrases("toadstool")
+#all_phrases('link')
